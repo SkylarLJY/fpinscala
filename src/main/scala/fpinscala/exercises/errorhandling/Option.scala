efd60d7bd7d7e066ffca9f6bc4a77d2b06342bbf
Skylar Liang <skylarliang233@gmail.com>
2023-05-22 15:09:43 -0400

    error handling: Option

--- src/main/scala/fpinscala/exercises/errorhandling/Option.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Option.scala
@@ -7,15 +7,21 @@ enum Option[+A]:
   case Some(get: A)
   case None
 
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] = this match
+    case None => None
+    case Some(a) => Some(f(a))
+   
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B>:A](default: => B): B = this match
+    case None => default
+    case Some(a) => a
+  
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] = map(Some(_)).getOrElse(ob)
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] = flatMap(a=>if f(a) then Some(a) else None)
 
 object Option:
 
@@ -36,10 +42,22 @@ object Option:
     if xs.isEmpty then None
     else Some(xs.sum / xs.length)
 
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: Seq[Double]): Option[Double] = 
+    mean(xs).flatMap(valM => mean(xs.map(x => math.pow(x-valM, 2))))
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = 
+    a.flatMap(x => b.map(y => f(x,y)))
 
-  def sequence[A](as: List[Option[A]]): Option[List[A]] = ???
+  def sequence[A](as: List[Option[A]]): Option[List[A]] = 
+    as.foldRight[Option[List[A]]](Some(Nil))((a, acc) => map2(a, acc)(_::_))
+    // as.foldRight(Some(List[A]()))((a, b)=> (a, b) match
+    //   case (Some(aa), Some(acc)) => Some(aa::acc)
+    //   case _ => None
+    // )
 
-  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = ???
+
+  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = 
+    as.foldRight[Option[List[B]]](Some(Nil))((a, acc)=>map2(f(a), acc)(_::_))
+
+  def seqWithTraverse[A](as: List[Option[A]]): Option[List[A]] = 
+    traverse(as)(a=>a)
